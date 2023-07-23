91626a7b69b902bcacdd2bc7f7fab309a8989db7
박진성 <plashdof@gmail.com>
2023-07-23 17:17:45 +0900

    #21 Home 주변화장실 api 연결

--- app/src/main/java/com/android/chamma/models/homemodel/NearToiletResponse.kt
+++ app/src/main/java/com/android/chamma/models/homemodel/NearToiletResponse.kt
@@ -1,6 +1,6 @@
 package com.android.chamma.models.homemodel
 
-data class LocationResponse (
+data class NearToiletResponse (
     val code : String,
     val msg : String,
     val data : ArrayList<MarkerData>
@@ -10,7 +10,8 @@ data class MarkerData(
     val restroomName : String,
     val longitude : Double,
     val latitude : Double,
-    val rating : Float,
+    val address : String,
+    val publicOrPaid : String,
+    val reviewRating : Float,
     val distance : Double,
-    val publicOrPaid : String
 )
--- app/src/main/java/com/android/chamma/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/chamma/ui/home/HomeFragment.kt
@@ -11,19 +11,21 @@ import com.android.chamma.R
 import com.android.chamma.config.BaseFragmentVB
 import com.android.chamma.databinding.FragmentHomeBinding
 import com.android.chamma.models.homemodel.MarkerData
+import com.android.chamma.models.homemodel.NearToiletResponse
+import com.android.chamma.ui.home.network.NearToiletAPI
 import com.android.chamma.ui.main.MainActivity
 import com.android.chamma.ui.search.SearchFragment
 import com.android.chamma.util.Constants.TAG
-import com.android.chamma.util.ToastMessageUtil.showToast
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.android.chamma.util.RetrofitInterface
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.math.*
 
 class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home), OnMapReadyCallback {
 
@@ -31,9 +33,14 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
     private lateinit var mapView : MapView
     private lateinit var naverMap : NaverMap
     private lateinit var locationSource : FusedLocationSource
+    private lateinit var lastPosition  : Pair<Double,Double>
 
     private val LOCATION_PERMISSTION_REQUEST_CODE = 1000
     private var locationState = true
+    private var toiletState = "entire"
+    private val markerList = arrayListOf<Marker>()
+
+    private var swiping = false
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -41,6 +48,7 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         binding.etSearch.setOnFocusChangeListener  { view, hasFocus ->
             if(hasFocus){
@@ -62,29 +70,38 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
         naverMap.uiSettings.isZoomControlEnabled = false
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.minZoom = 10.0
         locationBtnListener()
         toiletBtnListener()
 
         // 화면 이동시 리스너
         naverMap.addOnCameraChangeListener{ reason, animated ->
-            Log.d("NaverMap", "카메라 변경 - latitude : ${naverMap.cameraPosition.target.latitude}" +
-                    "longitude : ${naverMap.cameraPosition.target.longitude}")
+            // 스와이프 시작하면, 시작위치 저장
+            if(!swiping){
+                swiping = true
+                lastPosition = Pair(naverMap.cameraPosition.target.latitude,naverMap.cameraPosition.target.longitude)
+            }
         }
 
+        naverMap.addOnCameraIdleListener {
+            swiping = false
+            // 스와이프 멈추면, 해당 위치에서 마커찍기
+            // 300m 이상 스와이프 했을때만
+            if(getDistance(lastPosition.first,
+                    lastPosition.second,
+                    naverMap.cameraPosition.target.latitude,naverMap.cameraPosition.target.longitude) > 300){
 
-        // 서울시청역 부근 테스트 데이터
-        val d1 = MarkerData("테스트공중화장실1",126.9783740,37.5670135,4.5F,100.0,"무료화장실")
-        val d2 = MarkerData("테스트유료화장실1",126.9783740,37.5400,4.0F,110.0,"유료화장실")
-        val d3 = MarkerData("테스트공중화장실2",126.9783740,37.5500,3.5F,150.0,"무료화장실")
-        val d4 = MarkerData("테스트유료화장실2",126.9783740,37.5300,3.5F,2000.0,"유료화장실")
-        val d5 = MarkerData("테스트공중화장실3",126.9883740,37.5500,4.98F,120.0,"무료화장실")
-        val d6 = MarkerData("테스트유료화장실3",126.9883740,37.5300,1.5F,170.0,"유료화장실")
+                nearToiletList(toiletState,naverMap.cameraPosition.target.longitude,naverMap.cameraPosition.target.latitude,2000.0)
+            }
+        }
 
-        val freedatas = arrayListOf(d1,d3,d5)
-        val paydatas = arrayListOf(d2,d4,d6)
+        naverMap.addOnLocationChangeListener {
+            Log.d(TAG,"location change")
+            nearToiletList("entire"
+                ,it.longitude
+                ,it.latitude)
+        }
 
-        setFreetoiletMarker(freedatas)
-        setPaytoiletMarker(paydatas)
     }
 
     private fun locationBtnListener(){
@@ -92,10 +109,12 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
         // 현위치 tracking 활성화 / 비활성화 버튼
         binding.btnLocation.setOnClickListener {
             if(locationState){
+                naverMap.minZoom = 10.0
                 naverMap.locationTrackingMode = LocationTrackingMode.None
                 locationState = false
                 binding.btnLocation.setImageResource(R.drawable.home_location_btn)
             }else{
+                naverMap.minZoom = 15.0
                 naverMap.locationTrackingMode = LocationTrackingMode.Follow
                 locationState = true
                 binding.btnLocation.setImageResource(R.drawable.home_location_btn_on)
@@ -115,6 +134,11 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
 
             binding.btnPaytoilet.setBackgroundResource(R.drawable.shape_home_topbtn_white)
             binding.btnPaytoilet.setTextColor(ContextCompat.getColor(App.context(),R.color.chamma_gray))
+            toiletState = "entire"
+            removeMarker()
+            nearToiletList("entire"
+                ,naverMap.cameraPosition.target.longitude
+                ,naverMap.cameraPosition.target.latitude)
         }
 
         binding.btnFreetoilet.setOnClickListener {
@@ -126,6 +150,11 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
 
             binding.btnPaytoilet.setBackgroundResource(R.drawable.shape_home_topbtn_white)
             binding.btnPaytoilet.setTextColor(ContextCompat.getColor(App.context(),R.color.chamma_gray))
+            toiletState = "public"
+            removeMarker()
+            nearToiletList("public"
+                ,naverMap.cameraPosition.target.longitude
+                ,naverMap.cameraPosition.target.latitude)
         }
 
         binding.btnPaytoilet.setOnClickListener {
@@ -137,49 +166,88 @@ class HomeFragment : BaseFragmentVB<FragmentHomeBinding>(FragmentHomeBinding::bi
 
             binding.btnAlltoilet.setBackgroundResource(R.drawable.shape_home_topbtn_white)
             binding.btnAlltoilet.setTextColor(ContextCompat.getColor(App.context(),R.color.chamma_gray))
+            toiletState = "paid"
+            removeMarker()
+            nearToiletList("paid"
+                ,naverMap.cameraPosition.target.longitude
+                ,naverMap.cameraPosition.target.latitude)
         }
-
-
 
     }
 
-    private fun setFreetoiletMarker(datas : ArrayList<MarkerData>){
+    private fun nearToiletList(type : String, longitude : Double, latitude : Double,distance : Double?=null){
 
-        for(data in datas){
-            val marker = Marker()
-            marker.position = LatLng(data.latitude,data.longitude)
-            marker.icon = OverlayImage.fromResource(R.drawable.home_marker_freetoilet)
-            marker.map = naverMap
+        removeMarker()
+        RetrofitInterface.retrofit.create(NearToiletAPI::class.java)
+            .getNearToilet(type,longitude = longitude, latitude = latitude, distance = distance).enqueue(object : Callback<NearToiletResponse>{
+                override fun onResponse(
+                    call: Call<NearToiletResponse>,
+                    response: Response<NearToiletResponse>
+                ) {
+                    if(response.code() == 200){
 
-            marker.setOnClickListener {
+                        response.body()?.data?.forEach{
+                            if(it.publicOrPaid == "public") setFreetoiletMarker(it)
+                            else setPaytoiletMarker(it)
+                        }
+                    }
+                }
 
-                HomeBottomSheet(data).show(parentFragmentManager, "HomeBottomSheet")
+                override fun onFailure(call: Call<NearToiletResponse>, t: Throwable) {
+                    Log.d(TAG,"${t.message}")
+                }
+            })
 
-                true
-            }
-        }
     }
 
-    private fun setPaytoiletMarker(datas : ArrayList<MarkerData>){
+    private fun setFreetoiletMarker(data : MarkerData){
 
-        for(data in datas){
-            val marker = Marker()
-            marker.position = LatLng(data.latitude,data.longitude)
-            marker.icon = OverlayImage.fromResource(R.drawable.home_marker_paytoilet)
-            marker.map = naverMap
+        val marker = Marker()
+        marker.position = LatLng(data.latitude,data.longitude)
+        marker.icon = OverlayImage.fromResource(R.drawable.home_marker_freetoilet)
+        marker.map = naverMap
 
-            marker.setOnClickListener {
+        marker.setOnClickListener {
 
-                HomeBottomSheet(data).show(parentFragmentManager, "HomeBottomSheet")
+            HomeBottomSheet(data).show(parentFragmentManager, "HomeBottomSheet")
 
-                true
-            }
+            true
         }
+
+        markerList.add(marker)
+    }
+
+    private fun setPaytoiletMarker(data : MarkerData){
+
+        val marker = Marker()
+        marker.position = LatLng(data.latitude,data.longitude)
+        marker.icon = OverlayImage.fromResource(R.drawable.home_marker_paytoilet)
+        marker.map = naverMap
+
+        marker.setOnClickListener {
+
+            HomeBottomSheet(data).show(parentFragmentManager, "HomeBottomSheet")
+
+            true
+        }
+
+        markerList.add(marker)
     }
 
     private fun removeMarker(){
-        val marker = Marker()
-        marker.map = null
+        (context as MainActivity).runOnUiThread {
+            markerList.forEach{it.map = null}
+            markerList.clear()
+        }
+    }
+
+    private fun getDistance(lat1 : Double, lon1 : Double, lat2 : Double, lon2 : Double) : Int{
+        val R = 6372.8 * 1000
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = sin(dLat / 2).pow(2.0) + sin(dLon / 2).pow(2.0) * cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2))
+        val c = 2 * asin(sqrt(a))
+        return (R * c).toInt()
     }
 
 
