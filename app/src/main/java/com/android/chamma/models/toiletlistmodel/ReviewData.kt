49aba335444e32d2fe348fa7a7f4456bae38f4e1
hhj <sbs98010@naver.com>
2023-08-01 14:57:29 +0900

    #13 조금 더함 어댑터 전

---
+++ app/src/main/java/com/android/chamma/models/toiletlistmodel/ReviewData.kt
@@ -0,0 +1,11 @@
+package com.android.chamma.models.toiletlistmodel
+
+data class ReviewData (
+    val Nickname:String,
+    val year:Int,
+    val month:Int,
+    val day:Int,
+    val starNum:Int,
+    val content:String
+
+    )
--- app/src/main/java/com/android/chamma/ui/toiletlist/ReviewActivity.kt
+++ app/src/main/java/com/android/chamma/ui/toiletlist/ReviewActivity.kt
@@ -2,16 +2,37 @@ package com.android.chamma.ui.toiletlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.ImageButton
+import androidx.activity.result.contract.ActivityResultContracts
 import com.android.chamma.R
 import com.android.chamma.config.BaseActivityVB
 import com.android.chamma.databinding.ActivityReviewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ReviewActivity : BaseActivityVB<ActivityReviewBinding>(ActivityReviewBinding::inflate) {
+    private lateinit var bottomSheet: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.optionBtn.setOnClickListener{
-            
+            bottomSheet = layoutInflater.inflate(R.layout.fragment_btmshtdialog_sort_list, null)
+            val bottomSheetDialog = BottomSheetDialog(this)
+            val btnClose = bottomSheet.findViewById<Button>(R.id.btmshtBtnClose)
+
+
+            // calendar bottomsheetdialog 설정
+            bottomSheetDialog.setContentView(bottomSheet)
+            bottomSheetDialog.show()
+
+            btnClose.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+
         }
     }
 }
