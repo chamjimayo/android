4344f0ec71502f6a1c57866513dca4d25a7ea1ce
hhj <sbs98010@naver.com>
2023-08-04 01:09:38 +0900

    #39 ui 작업 중

---
+++ app/src/main/java/com/android/chamma/ui/toiletlist/RestroomVPAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.weeks6
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.chamma.databinding.LayoutRestroomViewpagerItemBinding
+
+class RestroomVPAdapter (private var pageList: ArrayList<Int>) : RecyclerView.Adapter<RestroomPagerViewHolder>(){
+
+/*
+    inner class ViewHolder(val binding: LayoutIntroPagerItemBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(data:PageItem){
+            binding.pagerItemImage.setImageResource(data.imageSrc)
+            binding.pagerItemBg.setBackgroundResource(data.bgColor)
+        }
+    }
+ */
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestroomPagerViewHolder {
+        return RestroomPagerViewHolder( LayoutRestroomViewpagerItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return pageList.size
+    }
+
+    override fun onBindViewHolder(holder: RestroomPagerViewHolder, position: Int) {
+        holder.bindWithView(pageList[position])
+    }
+
+}
--- app/src/main/java/com/android/chamma/ui/toiletlist/ToiletlistFragment.kt
+++ app/src/main/java/com/android/chamma/ui/toiletlist/ToiletlistFragment.kt
@@ -2,20 +2,53 @@ package com.android.chamma.ui.toiletlist
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.android.chamma.R
 import com.android.chamma.config.BaseFragmentVB
 import com.android.chamma.databinding.FragmentToiletListBinding
+import com.example.weeks6.RestroomVPAdapter
+import me.relex.circleindicator.CircleIndicator3
 
 class ToiletlistFragment : BaseFragmentVB<FragmentToiletListBinding>(FragmentToiletListBinding::bind, R.layout.fragment_toilet_list){
+    private var pageItemList = ArrayList<Int>()
+    private lateinit var RestroomVPAdapter: RestroomVPAdapter
+
+   // private val viewBinding: FragmentToiletListBinding by lazy{
+        //FragmentToiletListBinding.inflate(layoutInflater)
+    //}
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        pageItemList.add(R.drawable.restroom_ex)
+        pageItemList.add(R.drawable.restroom_ex)
+        pageItemList.add(R.drawable.restroom_ex)
+        pageItemList.add(R.drawable.restroom_ex)
 
-        binding.reviewTv.setOnClickListener {
+        RestroomVPAdapter=RestroomVPAdapter(pageItemList)
+
+        binding.restroomVp.apply {
+            adapter=RestroomVPAdapter
+            orientation= ViewPager2.ORIENTATION_HORIZONTAL
+        }
+
+        val indicator: CircleIndicator3 =binding.indicator
+        indicator.setViewPager(binding.restroomVp)
+
+        binding.useBtn.setOnClickListener {
             val intent= Intent(requireContext(), ReviewActivity::class.java)
             startActivity(intent)
         }
-
     }
+
 }
