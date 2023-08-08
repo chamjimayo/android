f89c1ae7d4e461c21938d37d0254da3bc88039f8
박진성 <plashdof@gmail.com>
2023-08-08 19:24:26 +0900

    #56 로그인시 권한설정

--- app/src/main/java/com/umc/chamma/ui/toiletlist/ToiletlistFragment.kt
+++ app/src/main/java/com/umc/chamma/ui/toiletlist/ToiletlistFragment.kt
@@ -1,17 +1,31 @@
 package com.umc.chamma.ui.toiletlist
 
+
 import android.os.Bundle
 import android.view.View
 import com.umc.chamma.R
 import com.umc.chamma.config.BaseFragmentVB
 import com.umc.chamma.databinding.FragmentToiletListBinding
+import com.umc.chamma.ui.home.main.HomeFragmentInterface
+import com.umc.chamma.ui.home.main.HomeService
+import com.umc.chamma.ui.home.model.NearToiletResponse
 
 
-class ToiletlistFragment : BaseFragmentVB<FragmentToiletListBinding>(FragmentToiletListBinding::bind, R.layout.fragment_toilet_list){
+class ToiletlistFragment : BaseFragmentVB<FragmentToiletListBinding>(FragmentToiletListBinding::bind, R.layout.fragment_toilet_list), HomeFragmentInterface{
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
        super.onViewCreated(view, savedInstanceState)
+//        HomeService(this).getNearToilet("entire",)
 
    }
+
+
+    override fun onGetNearToiletSuccess(result: NearToiletResponse) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGetNearToiletFailure(message: String) {
+        TODO("Not yet implemented")
+    }
 
 
 
