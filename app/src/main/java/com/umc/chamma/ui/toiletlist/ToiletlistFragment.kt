31a219f6a378ad3f84bf71391c44c05890b46562
박진성 <plashdof@gmail.com>
2023-08-09 16:11:37 +0900

    #56 화장실리스트 adapter 연결

--- app/src/main/java/com/umc/chamma/ui/toiletlist/ToiletlistFragment.kt
+++ app/src/main/java/com/umc/chamma/ui/toiletlist/ToiletlistFragment.kt
@@ -5,12 +5,16 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.naver.maps.map.LocationTrackingMode
 import com.umc.chamma.R
 import com.umc.chamma.config.App
@@ -19,12 +23,17 @@ import com.umc.chamma.databinding.FragmentToiletListBinding
 import com.umc.chamma.ui.home.main.HomeFragmentInterface
 import com.umc.chamma.ui.home.main.HomeService
 import com.umc.chamma.ui.home.model.NearToiletResponse
+import com.umc.chamma.ui.home.restroomInfo.RestroomInfoActivity
 import com.umc.chamma.ui.main.MainActivity
+import com.umc.chamma.ui.search.adapter.RecentKeywordAdapter
+import com.umc.chamma.ui.toiletlist.adapter.ToiletListAdapter
 import com.umc.chamma.util.Constants
 import com.umc.chamma.util.Constants.TAG
 
 
 class ToiletlistFragment : BaseFragmentVB<FragmentToiletListBinding>(FragmentToiletListBinding::bind, R.layout.fragment_toilet_list), HomeFragmentInterface{
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
        super.onViewCreated(view, savedInstanceState)
 
@@ -39,22 +48,33 @@ class ToiletlistFragment : BaseFragmentVB<FragmentToiletListBinding>(FragmentToi
                 ActivityCompat.checkSelfPermission(App.context(), Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED
             ) {
-                val loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//                HomeService(this).getNearToilet("entire",loc!!.longitude, loc.latitude)
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity as MainActivity)
+                fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+//                    HomeService(this).getNearToilet("entire", location!!.longitude, location.latitude)
+                    
+                    //TODO 인천에 화장실 데이터 없어서 하드코딩 테스트
+                    HomeService(this).getNearToilet("paid", 126.9731649095934, 37.560444374518106)
+                }
             }else{
                 ActivityCompat.requestPermissions((activity as MainActivity), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), Constants.RC_PERMISSION)
             }
 
         }
-
    }
+    private fun clickItem(toiletId : Int){
+        val intent = Intent(App.context(),RestroomInfoActivity::class.java)
+            .putExtra("ID",toiletId)
+        startActivity(intent)
+    }
 
     override fun onGetNearToiletSuccess(result: NearToiletResponse) {
-        TODO("Not yet implemented")
+
+        val adapter = ToiletListAdapter(result.data,::clickItem)
+        binding.recycler.adapter = adapter
+        binding.recycler.layoutManager = LinearLayoutManager(App.context())
     }
 
     override fun onGetNearToiletFailure(message: String) {
-        TODO("Not yet implemented")
     }
 
 
