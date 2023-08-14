426b984ef9f0e25ef6d8925b8fd6ea1f3d66cc84
박진성 <plashdof@gmail.com>
2023-08-14 11:20:17 +0900

    fix tissuimg name

--- app/src/main/java/com/umc/chamma/ui/mypage/chargepoint/ChargePointActivity.kt
+++ app/src/main/java/com/umc/chamma/ui/mypage/chargepoint/ChargePointActivity.kt
@@ -10,11 +10,13 @@ import com.umc.chamma.util.InappUtil
 
 class ChargePointActivity : BaseActivityVB<ActivityChargepointBinding>(ActivityChargepointBinding::inflate), ChargePointActivityInterface {
 
-    private val inappUtil by lazy{InappUtil(this)}
+
+    private var inappUtil : InappUtil? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        inappUtil.initBillingClient(this)
+        inappUtil = InappUtil(this)
+        inappUtil?.initBillingClient(this)
         setBtnListener()
         setUserPoint()
 
@@ -25,11 +27,12 @@ class ChargePointActivity : BaseActivityVB<ActivityChargepointBinding>(ActivityC
     }
 
     private fun setBtnListener(){
-        binding.btn1000Charge.setOnClickListener { inappUtil.getPay(this,"point_1000") }
-        binding.btn3000Charge.setOnClickListener { inappUtil.getPay(this,"point_3000") }
-        binding.btn5000Charge.setOnClickListener { inappUtil.getPay(this,"point_5000") }
-        binding.btn8000Charge.setOnClickListener { inappUtil.getPay(this,"point_8000")  }
-        binding.btn10000Charge.setOnClickListener { inappUtil.getPay(this,"point_10000") }
+
+        binding.btn1000Charge.setOnClickListener { inappUtil?.getPay(this,"point_1000") }
+        binding.btn3000Charge.setOnClickListener { inappUtil?.getPay(this,"point_3000") }
+        binding.btn5000Charge.setOnClickListener { inappUtil?.getPay(this,"point_5000") }
+        binding.btn8000Charge.setOnClickListener { inappUtil?.getPay(this,"point_8000")  }
+        binding.btn10000Charge.setOnClickListener { inappUtil?.getPay(this,"point_10000") }
         binding.btnBackCharge.setOnClickListener { finish() }
     }
 
@@ -40,6 +43,21 @@ class ChargePointActivity : BaseActivityVB<ActivityChargepointBinding>(ActivityC
 
     override fun onGetUserInfoFailure(message: String) {
         showCustomToast(message)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        inappUtil = null
+    }
+
+    override fun onStop() {
+        super.onStop()
+        inappUtil = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        inappUtil = null
     }
 
 
