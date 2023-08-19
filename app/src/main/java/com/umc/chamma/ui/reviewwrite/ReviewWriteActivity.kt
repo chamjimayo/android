2d601c410d9f2464bc921fe6f9b10b629bcbf12d
박진성 <plashdof@gmail.com>
2023-08-19 22:52:32 +0900

    #85 리뷰작성 셋팅

---
+++ app/src/main/java/com/umc/chamma/ui/reviewwrite/ReviewWriteActivity.kt
@@ -0,0 +1,13 @@
+package com.umc.chamma.ui.reviewwrite
+
+import android.os.Bundle
+import com.umc.chamma.config.BaseActivityVB
+import com.umc.chamma.databinding.ActivityReviewwriteBinding
+
+class ReviewWriteActivity : BaseActivityVB<ActivityReviewwriteBinding>(ActivityReviewwriteBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
