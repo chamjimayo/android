b53be3e4cf5350e8a7fc94ffb4b602246a3c83f4
hhj <sbs98010@naver.com>
2023-08-22 18:18:46 +0900

    #116 커뮤니티 화면 보이게끔만

--- app/src/main/java/com/umc/chamma/ui/community/CommunityFragment.kt
+++ app/src/main/java/com/umc/chamma/ui/community/CommunityFragment.kt
@@ -11,19 +11,19 @@ import com.umc.chamma.ui.home.restroomInfo.ReviewDataAdapter
 class CommunityFragment : com.umc.chamma.config.BaseFragmentVB<FragmentCommunityBinding>(FragmentCommunityBinding::bind, R.layout.fragment_community){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
- val dataList:ArrayList<CommunityData> = arrayListOf()
+     val dataList:ArrayList<CommunityData> = arrayListOf()
         dataList.apply{
             add(
-                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
+                CommunityData(13,R.drawable.community_1_1,R.drawable.community_1_2,5632,
                     "화장실에서 샤워할 때, 나의 노래 타입은?")
             )
             add(
-                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
-                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+                CommunityData(28,R.drawable.community_2_1,R.drawable.community_2_2,3234,
+                    "아침에 일어난 당신, 양치는 언제?")
             )
             add(
-                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
-                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+                CommunityData(52,R.drawable.community_3_1,R.drawable.community_3_2,1243,
+                    "출근해야하는데 늦잠 잔 상황! 당신의 선택은?")
             )
             add(
                 CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
