369192f806d6f47e0f54a033670a95dbf6a84319
hhj <sbs98010@naver.com>
2023-08-22 17:24:46 +0900

    #116 어댑터 연결중

--- app/src/main/java/com/umc/chamma/ui/community/CommunityFragment.kt
+++ app/src/main/java/com/umc/chamma/ui/community/CommunityFragment.kt
@@ -2,14 +2,36 @@ package com.umc.chamma.ui.community
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.chamma.R
 import com.umc.chamma.config.BaseFragmentVB
 import com.umc.chamma.databinding.FragmentCommunityBinding
+import com.umc.chamma.ui.home.restroomInfo.ReviewDataAdapter
 
 class CommunityFragment : com.umc.chamma.config.BaseFragmentVB<FragmentCommunityBinding>(FragmentCommunityBinding::bind, R.layout.fragment_community){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+ val dataList:ArrayList<CommunityData> = arrayListOf()
+        dataList.apply{
+            add(
+                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
+                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+            )
+            add(
+                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
+                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+            )
+            add(
+                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
+                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+            )
+            add(
+                CommunityData(5362,R.drawable.community_1_1,R.drawable.community_1_2,13,
+                    "화장실에서 샤워할 때, 나의 노래 타입은?")
+            )
+        }
+        binding.recyclerView.adapter= CommunityDataAdapter(dataList)
+        binding.recyclerView.layoutManager= LinearLayoutManager(context)
     }
 
 }
